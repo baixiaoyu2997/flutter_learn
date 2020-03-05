891335a8accd05f85d6765bd47aff1ea70e336b5
L.Rain <bxy2997@sina.com>
2020-03-05 23:42:17 +0800

    添加本地化

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-// Flutter code sample for BottomNavigationBar
-
-// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
-// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
-// widgets and the [currentIndex] is set to index 0. The selected item is
-// amber. The `_onItemTapped` function changes the selected item's index
-// and displays a corresponding message in the center of the [Scaffold].
-//
-// ![A scaffold with a bottom navigation bar containing three bottom navigation
-// bar items. The first one is selected.](https://flutter.github.io/assets-for-api-docs/assets/material/bottom_navigation_bar.png)
-
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'login.dart';
+
 void main() => runApp(MyApp());
 
 /// This Widget is the main application widget.
@@ -20,6 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        // 本地化的代理类
+        GlobalMaterialLocalizations.delegate, // 提供的本地化的字符串和其他值，它可以使Material 组件支持多语言
+        GlobalWidgetsLocalizations.delegate, // 定义组件默认的文本方向
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('zh', 'CN'), // 中文简体
+        //其它Locales
+      ],
       title: _title,
       home: LoginWidget(),
     );
